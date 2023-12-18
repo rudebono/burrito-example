ba1d506a3e1b6553124ed2e89d861eea64ee61f3
Hany Rock <rock@momenti.tv>
2023-12-18 16:02:28 +0900

    Add releases

--- mix.exs
+++ mix.exs
@@ -1,27 +1,42 @@
 defmodule BurritoExample.MixProject do
   use Mix.Project
 
-  def project do
+  def project() do
     [
       app: :burrito_example,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: releases()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
       {:burrito, "~> 1.0"}
+    ]
+  end
+
+  def releases() do
+    [
+      burrito_example: [
+        steps: [:assemble, &Burrito.wrap/1],
+        burrito: [
+          targets: [
+            linux_aarch64: [os: :linux, cpu: :aarch64],
+            linux_x86_64: [os: :linux, cpu: :x86_64],
+            macos_aarch64: [os: :darwin, cpu: :aarch64],
+            macos_x86_64: [os: :darwin, cpu: :x86_64]
+          ]
+        ]
+      ]
     ]
   end
 end
